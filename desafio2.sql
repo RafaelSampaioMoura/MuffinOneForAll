d828ab86485f09cec6e898d693cdaddf4fccb78b
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2022-12-04 22:24:01 -0300

    Desafio 01 e 02

---
+++ desafio2.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(c.cancao_id) AS cancoes, 
+	COUNT(DISTINCT ar.artista_id) AS artistas, 
+    COUNT(DISTINCT al.album_id) AS albuns
+FROM SpotifyClone.Cancoes AS c
+INNER JOIN SpotifyClone.Albums AS al ON c.album_id = al.album_id
+INNER JOIN SpotifyClone.Artistas AS ar ON ar.artista_id = al.artista_id;
